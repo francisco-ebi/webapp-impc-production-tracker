cf0f308cbafef559d73005b2d3e435cabdf8eda6
Jose Francisco Peña Estrada <francisco@ebi.ac.uk>
2026-05-11 16:33:15 +0100

    Merge pull request #715 from francisco-ebi/feat/fix-security-issues
    Fix security issues

--- src/app/feature-modules/users/users-routing.module.ts
+++ src/app/feature-modules/users/users-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ProfileEditorComponent } from './components/profile-editor/profile-editor.component';
+import { AuthGuard } from 'src/app/core/guards/auth.guard';
 
 
 const routes: Routes = [
   {
-    path: 'edit_profile', component: ProfileEditorComponent,
+    path: 'edit_profile', component: ProfileEditorComponent, canActivate: [AuthGuard],
   }
 ];
 
